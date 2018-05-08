f36255fb14316766c5d45b714a08c8bbbf651b68
Milena Suarez <milena.suarez.luna@gmail.com>
2018-05-07 20:54:52 -0500

    add expensive component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExpenseComponent } from './expense/expense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/expense/expense.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense',
+  templateUrl: './expense.component.html',
+  styleUrls: ['./expense.component.css']
+})
+export class ExpenseComponent implements OnInit {
+
+
+  genderList = ['otp', 'pin', 'others'];
+
+  ngOnInit() {
+  }
+
+}
+
