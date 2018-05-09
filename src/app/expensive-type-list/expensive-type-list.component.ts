dd777f303a236645fc1ed37419369264f90ad798
Milena Suarez <milena.suarez.luna@gmail.com>
2018-05-08 22:21:19 -0500

    fix form and add main mock

--- src/app/expensive-type-list/expensive-type-list.component.ts
+++ src/app/expensive-type-list/expensive-type-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-expensive-type-list',
   templateUrl: './expensive-type-list.component.html',
---
+++ src/app/mocks/transactions.mock.ts
@@ -0,0 +1,47 @@
+export class TransactionMocks {
+    public transactions: any[] = [{
+        idRank: 1,
+        transactionId: 1,
+        transactionName: 'PAGO FACTURAS TC',
+        frequency: 1,
+        amountFloor: '0',
+        amountCeil: '200000',
+        transactionRegisterRequired: false,
+        deviceRegisterRequired: true,
+        deviceRegisterLock: false,
+        notificationrequired: true,
+        factors: [
+            {
+                id: 1,
+                name: 'USUARIO Y CONTRASEÑA',
+                frecuency: 'CADA VEZ'
+            }
+        ],
+        userName: 'System'
+    },
+    {
+        idRank: 2,
+        transactionId: 1,
+        transactionName: 'PAGO IMPUESTOS TC',
+        frequency: 1,
+        amountFloor: '0',
+        amountCeil: '999999999999999999',
+        transactionRegisterRequired: false,
+        deviceRegisterRequired: true,
+        deviceRegisterLock: false,
+        notificationrequired: true,
+        factors: [
+            {
+                id: 1,
+                name: 'USUARIO Y CONTRASEÑA',
+                frecuency: 'CADA VEZ'
+            },
+            {
+                id: 2,
+                name: 'OTP',
+                frecuency: 'CADA VEZ'
+            }
+        ],
+        userName: 'System'
+    }];
+}
